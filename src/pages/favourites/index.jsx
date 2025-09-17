31f6d82d6fe5754f6f18c4f12875a34bf6a861c4
Sriram1723 <sriramgodhuma@gmail.com>
2025-09-18 01:52:20 +0530

    added code

--- src/pages/favourites/index.jsx
+++ src/pages/favourites/index.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react"
 import { GlobalContext } from "../../context"
 import {Link} from 'react-router-dom'
-import {FaTrash} from 'react-icons/fa'
+import {FaTrash} from 'react-icons/fa6'
 
 
 export default function Favourites(){
